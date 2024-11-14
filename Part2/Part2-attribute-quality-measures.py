ab0c9e5619fd98ad5469584e1c17c1215ce83b80
Toby Towler <tobytowler11@gmail.com>
2024-11-14 15:38:26 +0000

    fisrt

---
+++ Part2/Part2-attribute-quality-measures.py
@@ -0,0 +1,16 @@
+import math
+
+HeadacheDiagnosis = [[3, 0], [2, 3]]
+SpotsDiagnosis = [[4, 1], [1, 2]]
+StiffneckDiagnosis = [[4, 1], [1, 2]]
+
+
+def get_gain(contingency_table, any_other_parameter):
+    gain = 
+    return gain
+# def get_gini(contingency_table, any_other_parameter):
+# gini =
+# return gini
+# def get_chi(contingency_table, any_other_parameter):
+# x2 =
+# return x2
